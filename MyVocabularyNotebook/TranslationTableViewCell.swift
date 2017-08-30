9fd0e5daded478767fda73648914324d20cbe8fb
adam <kisferencadam@gmail.com>
2017-08-30 11:00:03 +0200

    Table view cells - initial

---
+++ MyVocabularyNotebook/TranslationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TranslationTableViewCell.swift
+//  MyVocabularyNotebook
+//
+//  Created by Adam on 2017. 08. 30..
+//  Copyright © 2017. Adam. All rights reserved.
+//
+
+import UIKit
+
+class TranslationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
