54009a1e4258c73a9d1f29ea713a9de4fbc92258
adam <kisferencadam@gmail.com>
2017-09-01 14:30:12 +0200

    New translation object

---
+++ MyVocabularyNotebook/Translation.swift
@@ -0,0 +1,24 @@
+//
+//  Translation.swift
+//  MyVocabularyNotebook
+//
+//  Created by Adam on 2017. 09. 01..
+//  Copyright © 2017. Adam. All rights reserved.
+//
+
+import UIKit
+
+class Translation: NSObject {
+
+    // MARK: Properties
+    var sourceTranslation: String
+    var targetTranslation: String
+    var session: String!
+    
+    init(sourceTranslation: String, targetTranslation: String, session: String?){
+        self.sourceTranslation = sourceTranslation
+        self.targetTranslation = targetTranslation
+        self.session = session!
+    }
+    
+}
