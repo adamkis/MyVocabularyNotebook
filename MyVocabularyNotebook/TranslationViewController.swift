b5586e3c5e7e35306ad094b8868b6e593b7919f4
adam <kisferencadam@gmail.com>
2017-09-03 21:38:20 +0200

    Cancel button works

--- MyVocabularyNotebook/TranslationViewController.swift
+++ MyVocabularyNotebook/TranslationViewController.swift
@@ -46,6 +46,11 @@ class TranslationViewController: UIViewController, UITextViewDelegate {
     }
     
     // MARK: - Navigation
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
