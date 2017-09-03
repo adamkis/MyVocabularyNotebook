1dd174f7726783afb7a34da75c9b3674d4776d29
adam <kisferencadam@gmail.com>
2017-09-03 12:40:42 +0200

    Proper outlets

--- MyVocabularyNotebook/TranslationViewController.swift
+++ MyVocabularyNotebook/TranslationViewController.swift
@@ -15,23 +15,26 @@ class TranslationViewController: UIViewController {
 //    @IBOutlet weak var targetTranslationUITextView: UITextView!
 //    @IBOutlet weak var saveButtonNavBar: UIBarButtonItem!
 //    @IBOutlet weak var saveButton: UIButton!
-    
+    @IBOutlet weak var sourceTranslationView: UITextView!
+    @IBOutlet weak var targetTranslationView: UITextView!
+    @IBOutlet weak var saveButtonNav: UIBarButtonItem!
+    @IBOutlet weak var saveButton: UIButton!
     
     var translation: Translation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        let borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
-//        let borderWidth = CGFloat(1.0)
-//        let cornerRadius = CGFloat(5.0)
+        let borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
+        let borderWidth = CGFloat(1.0)
+        let cornerRadius = CGFloat(5.0)
         
-//        sourceTranslationUITextView.layer.borderColor = borderColor
-//        sourceTranslationUITextView.layer.borderWidth = borderWidth
-//        sourceTranslationUITextView.layer.cornerRadius = cornerRadius
-//        targetTranslationUITextView.layer.borderColor = borderColor
-//        targetTranslationUITextView.layer.borderWidth = borderWidth
-//        targetTranslationUITextView.layer.cornerRadius = cornerRadius
+        sourceTranslationView.layer.borderColor = borderColor
+        sourceTranslationView.layer.borderWidth = borderWidth
+        sourceTranslationView.layer.cornerRadius = cornerRadius
+        targetTranslationView.layer.borderColor = borderColor
+        targetTranslationView.layer.borderWidth = borderWidth
+        targetTranslationView.layer.cornerRadius = cornerRadius
         
     }
 
@@ -45,15 +48,15 @@ class TranslationViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
         // Configure the destination view controller only when the save button is pressed.
-//        guard let button = sender as? UIBarButtonItem, button === saveButtonNavBar,
-//            let button2 = sender as? UIButton, button2 === saveButton
-//            else {
-//                os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
-//                return
-//            }
-//        let sourceTranslation = sourceTranslationUITextView.text
-//        let targetTranslation = targetTranslationUITextView.text
-//        translation = Translation(sourceTranslation: sourceTranslation, targetTranslation: targetTranslation)
+        guard let button = sender as? UIBarButtonItem, button === saveButtonNav,
+            let button2 = sender as? UIButton, button2 === saveButton
+            else {
+                os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+                return
+            }
+        let sourceTranslation = sourceTranslationView.text
+        let targetTranslation = targetTranslationView.text
+        translation = Translation(sourceTranslation: sourceTranslation, targetTranslation: targetTranslation)
     }
 
 }
