40729c14c3ee7dcd37af3d0b415edf86941ffb6d
adam <kisferencadam@gmail.com>
2017-09-04 10:48:04 +0200

    Deleting works

--- MyVocabularyNotebook/TranslationTableViewController.swift
+++ MyVocabularyNotebook/TranslationTableViewController.swift
@@ -17,6 +17,9 @@ class TranslationTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem
+        
         let dummy1source = "das Haus das Haus das Haus das Haus das Haus das Haus das Haus"
         let dummy2source = "the house the house the house the house the house the house"
         
@@ -91,25 +94,22 @@ class TranslationTableViewController: UITableViewController {
     
     
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            translations.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
