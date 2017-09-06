c80610102a0ebc8a876d2f9c77e206106d9fa50f
adam <kisferencadam@gmail.com>
2017-09-06 10:43:02 +0200

    Rearranging list works

--- MyVocabularyNotebook/TranslationTableViewController.swift
+++ MyVocabularyNotebook/TranslationTableViewController.swift
@@ -131,20 +131,19 @@ class TranslationTableViewController: UITableViewController {
         }    
     }
 
-    /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+        let itemToMove:Translation = translations[fromIndexPath.row]
+        translations.remove(at: fromIndexPath.row)
+        translations.insert(itemToMove, at: to.row)
+        saveTranslations()
     }
-    */
 
-    /*
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
 
     // MARK: - Navigation
 
