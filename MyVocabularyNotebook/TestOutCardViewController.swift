85cb7f80a232fabc5de26a43ed6cc9a74d168bb9
adam <kisferencadam@gmail.com>
2017-10-23 21:55:34 +0200

    Pushing up card when keyboard is active

--- MyVocabularyNotebook/TestOutCardViewController.swift
+++ MyVocabularyNotebook/TestOutCardViewController.swift
@@ -27,6 +27,9 @@ class TestOutCardViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        NotificationCenter.default.addObserver(self, selector: #selector(TestOutCardViewController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(TestOutCardViewController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
         translationText.text = dummyString
         cardView.layer.cornerRadius = 25
         cardView.layer.masksToBounds = true
@@ -56,5 +59,24 @@ class TestOutCardViewController: UIViewController {
             self.backView.isHidden = false
         })
     }
+    
+    // MARK: handle keyboard
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0{
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0{
+                self.view.frame.origin.y += keyboardSize.height
+            }
+        }
+    }
+    
 }
 
